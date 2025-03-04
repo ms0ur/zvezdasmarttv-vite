c77bed9ea514607e8b5c060c910f0d4ececcac94
ms0ur <maksimsuslov7142@gmail.com>
2025-03-04 14:47:09 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './assets/styles/styles.scss'
+import {FirstSection} from "./components/first-section/FirstSection.tsx";
+
+function App() {
+
+  return (
+    <>
+      <FirstSection />
+    </>
+  )
+}
+
+export default App
