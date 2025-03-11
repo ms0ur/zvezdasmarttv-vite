a29b6eea6f3a27acf759002582a1827b903183c9
ms0ur <maksimsuslov7142@gmail.com>
2025-03-11 10:49:58 +0300

    Добавил свайпер

--- src/components/third-section/ThirdSection.tsx
+++ src/components/third-section/ThirdSection.tsx
@@ -2,10 +2,11 @@ import {ZButton} from "../";
 import {ProgramsGrid} from "../";
 
 import { useState, useEffect } from "react";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+// @ts-expect-error css from swiper npm package
+import 'swiper/css';
 
 import styles from './ThirdSection.module.scss';
-
 
 export function ThirdSection() {
     const [isTablet, setIsTablet] = useState(window.innerWidth < 992);
@@ -27,7 +28,28 @@ export function ThirdSection() {
                    <p className={styles.infoTextDesc}>Смотрите прямой эфир, фильмы, трансляции и новые выпуски программ первыми</p>
                </div>
                <div className={styles.infoGrid}>
-                   {isTablet ? <img width='100%' src="/programs/broadcast-15.png" alt=""/> : <ProgramsGrid />}
+                   {isTablet ? (
+                       <Swiper
+                           spaceBetween={50}
+                           slidesPerView={1}
+                       >
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-01.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-02.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-03.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-04.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-05.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-06.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-07.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-08.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-09.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-10.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-11.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-12.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-13.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-14.png" alt=""/></SwiperSlide>
+                           <SwiperSlide><img width='100%' src="/programs/broadcast-15.png" alt=""/></SwiperSlide>
+                       </Swiper>
+                   ) : <ProgramsGrid />}
                </div>
            </div>
            <div className={styles.action}>
